13168557a2698267ccad6a1db984aca50a7dd27a
Marta <marta.jastrzabek@gmail.com>
2019-07-25 16:16:17 +0200

    upgraded Scanner

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -4,34 +4,53 @@ public class GuessNumber {
 
     private int userNumber;
     private boolean condition;
+    private Scanner in;
 
+    public GuessNumber(Scanner in){
+        this.in = in;
+    }
 
     private void getUserInput(){
-        Scanner in = new Scanner(System.in);
         System.out.println("Podaj liczbę: ");
         userNumber = in.nextInt();
-        //in.close();
     }
 
     public void checkAndPrint(){
         condition = false;
         while (!condition){
             getUserInput();
-            if(userNumber>=100 && userNumber<=200){
-                if(userNumber%3==0){
-                    condition = true;
-                    System.out.println("Twoja liczba jest ok.");
-                }else {
-                    System.out.println("Liczba nie jest podzielna przez 3.");
-                }
-            }else if(userNumber<100){
-                System.out.println("Podana liczba jest za mała");
-            }else{
-                System.out.println("Podana Liczba jest za duża");
+            if(userNumber<100){
+                System.out.println("Twoja liczba jest za mała.");
+            } else if(userNumber>200){
+                System.out.println("Twoja liczba jest za duża.");
+            } else if (userNumber%3==0){
+                condition = true;
+                System.out.println("Twoja liczba jest ok");
+                break;
+            } else{
+                System.out.println("Twoja liczba nie jest podzielna przez 3.");
             }
         }
-
     }
 
+//    public void checkAndPrint(){
+//        condition = false;
+//        while (!condition){
+//            getUserInput();
+//            if(userNumber>=100 && userNumber<=200){
+//                if(userNumber%3==0){
+//                    condition = true;
+//                    System.out.println("Twoja liczba jest ok.");
+//                }else {
+//                    System.out.println("Liczba nie jest podzielna przez 3.");
+//                }
+//            }else if(userNumber<100){
+//                System.out.println("Podana liczba jest za mała");
+//            }else{
+//                System.out.println("Podana Liczba jest za duża");
+//            }
+//        }
+//
+//    }
 
 }
--- src/GuessNumberTest.java
+++ src/GuessNumberTest.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class GuessNumberTest {
     public static void main(String[] args) {
-        GuessNumber guess = new GuessNumber();
+        Scanner in = new Scanner(System.in);
+        GuessNumber guess = new GuessNumber(in);
         guess.checkAndPrint();
+        in.close();
     }
 }
